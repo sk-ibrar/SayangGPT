6b3ca94403889218e98e09dc213fc15c2cdaa179
Sk Ibrar Ahmed <skibrara@gmail.com>
2025-09-03 23:38:31 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import DashView from './views/dashView.vue';
+
+</script>
+
+<template>
+  <DashView />
+</template>
+
+<style scoped></style>
---
+++ src/views/dashView.vue
@@ -0,0 +1,8 @@
+<template>
+<Dashboard />
+</template>
+
+<script setup lang="ts">
+import Dashboard from '@/components/dashboard.vue';
+
+</script>
